d05133548a0cab2535389d782545a175437ac09a
alan-ocallaghan <serbulent.ocal@gmail.com>
2026-08-01 10:52:59 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# esirgeyen ve bağışlayan ❤️ Allah'ın (c.c) adıyla - 12a_react_official
+
+## Table of Contents
+* [Description](#description)
+
+This is a simple React application created with **Vite**. It demonstrates how to 
+- `conditionally render elements` with ternary operator based on certain conditions.
+`condition ? valueIfTrue : valueIfFalse`
+- Examples:
+* 1 - h2 element uses `{isLoggedIn ? "Welcome back!" : "Please log in"}`
+* 2- The button label `{isLoggedIn ? "Log Out" : "Log In"}`
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  //`useState` stores data that affects rendering.
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+
+      <div className="container">
+        <h1>Conditional Operator Example</h1>
+        <h2>
+          {isLoggedIn ? "Welcome back!" : "Please log in"}
+        </h2>
+
+        <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+          {isLoggedIn ? 'Log Out' : 'Log In'}
+        </button>
+      </div>
+  );
+}
+
+export default App
